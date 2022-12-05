09f5544b72f55eac704f538d96abbe1778eb90b5
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-12-05 21:53:14 +0100

    2022 - day 5

--- 2022/day05/main.jl
+++ 2022/day05/main.jl
@@ -27,9 +27,16 @@ function parsemove(line)
     return Move(parse(Int, r[1]), parse(Int, r[2]), parse(Int, r[3]))
 end
 
-function applymove!(stacks, move::Move)
+function applymove9000!(stacks, move::Move)
     for _ in 1:move.amount
         push!(stacks[move.to], pop!(stacks[move.from]))
+    end
+end
+
+function applymove9001!(stacks, move::Move)
+    n = size(stacks[move.from], 1) - move.amount + 1
+    for _ in 1:move.amount
+        push!(stacks[move.to], popat!(stacks[move.from], n))
     end
 end
 
@@ -37,13 +44,19 @@ function topstack(stacks)
     return mapfoldl(last, (a,b) -> string(a, b), stacks)
 end
 
-open("input") do file
-    lines = readlines(file)
-    sep = findfirst(isequal(""), lines)
-    stacks = readstacks(lines[1:sep - 2])
-    
-    for rawmove in view(lines, sep + 1:size(lines, 1))
-        applymove!(stacks, parsemove(rawmove))
+function solve(crane)
+    open("input") do file
+        lines = readlines(file)
+        sep = findfirst(isequal(""), lines)
+        stacks = readstacks(lines[1:sep - 2])
+        
+        for rawmove in view(lines, sep + 1:size(lines, 1))
+            crane(stacks, parsemove(rawmove))
+        end
+        println(topstack(stacks))
     end
-    println(topstack(stacks))
 end
+
+
+solve(applymove9000!)
+solve(applymove9001!)
