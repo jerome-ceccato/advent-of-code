4d02c8a7b18c25cbd3d0fbad9206694a9350f63c
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-12-16 23:35:05 +0100

    2022 - day16 p1

--- 2022/day16/main.nim
+++ 2022/day16/main.nim
@@ -3,7 +3,7 @@ import std/sugar
 import std/sequtils
 import std/tables
 import std/strformat
-import std/[times, os]
+import std/times
 import strutils
 
 # Types
@@ -12,12 +12,17 @@ type Valve = ref object
   pressure: int
   linked: seq[Valve]
 
-type Network = Table[string, Valve]
+type DistanceMemo = ref Table[string, Table[string, int]]
+
+type Network = ref object
+  nodes: Table[string, Valve]
+  memo: DistanceMemo
 
 type Simulation = object
   valve: Valve
   duration: int
   score: int
+
 
 # Input
 proc buildValveNetwork(filename: string): Network =
@@ -32,54 +37,55 @@ proc buildValveNetwork(filename: string): Network =
     valves[raw[0]] = Valve(name: raw[0], pressure: parseInt(raw[1]), linked: @[])
   for raw in parsed:
     valves[raw[0]].linked = raw[2].split(", ").map((n) => valves[n])
-  return valves
+  return Network(nodes: valves, memo: DistanceMemo())
+
 
 # Solution
-proc distance(self: Network, origin: Valve, target: Valve): int =
-  const unreachable = 9999999
-  var steps = 0
-  var visited = @[origin.name]
+proc distance(self: Network, origin: Valve): Table[string, int] =
+  if self.memo.contains(origin.name):
+    return self.memo[origin.name]
+
+  var steps = 1
+  var dist = to_table({origin.name: 0})
   var queue = @[origin]
 
-  while queue.len > 0 and not queue.contains(target):
+  while queue.len > 0:
     var next = newSeq[Valve]()
     for node in queue:
       for neighbor in node.linked:
-        if not visited.contains(neighbor.name):
-          visited.add(neighbor.name)
+        if not dist.contains(neighbor.name):
+          dist[neighbor.name] = steps
           next.add(neighbor)
     queue = next
     steps += 1
-  return if queue.len > 0: steps else: unreachable
+  
+  self.memo[origin.name] = dist
+  return dist
 
-
-proc totalPressureReleased(self: Network, current: Valve, target: Valve, time: int): Simulation =
-  let duration = self.distance(current, target) + 1
-
-  if duration > time:
-    return Simulation(valve: target, duration: 0, score: 0) # Can't reach and open the valve
-  return Simulation(valve: target, duration: duration, score: (time - duration) * target.pressure)
+proc totalPressureReleased(self: Network, current: Valve, candidates: seq[Valve], time: int): seq[Simulation] =
+  let distanceMap = self.distance(current)
+  return candidates.map(proc (v: Valve): Simulation =
+    let duration = distanceMap[v.name] + 1
+    if duration > time:
+      return Simulation(valve: v, duration: 0, score: 0) # Can't reach and open the valve
+    return Simulation(valve: v, duration: duration, score: (time - duration) * v.pressure)
+  )
 
 proc openValves(self: Network, current: Valve, closed: seq[Valve], time: int, depth: int): int =
   if time <= 0 or closed.len == 0:
     return 0
 
-  let simulations = closed.map((v) => self.totalPressureReleased(current, v, time))
+  let simulations = self.totalPressureReleased(current, closed, time)
   let all = simulations.filter((s) => s.score > 0).map do (sim: Simulation) -> int:
     if depth < 2:
       echo &"[{depth}] Moving to {sim.valve.name}: score({sim.score}) -> time({sim.duration})"
     sim.score + self.openValves(sim.valve, closed.filter((v) => v != sim.valve), time - sim.duration, depth + 1)
 
   return if all.len > 0: all.max() else: 0
-  #let best = simulations[simulations.map((s) => s.score).maxIndex]
-
-  #echo &"Moving to {best.valve.name}: score({best.score}) -> time({best.duration})"
-
-  #return best.score + self.openValves(best.valve, closed.filter((v) => v != best.valve), time - best.duration)
 
 
 # Main
 let time = cpuTime()
 let network = buildValveNetwork("input")
-echo network.openValves(network["AA"], toSeq(network.values).filter((v) => v.pressure > 0), 30, 0)
+echo network.openValves(network.nodes["AA"], toSeq(network.nodes.values).filter((v) => v.pressure > 0), 30, 0)
 echo "Time taken: ", cpuTime() - time
