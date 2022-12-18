fa369863cab9eba2ea3405f510d43b9eeb874fb8
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-12-19 00:16:54 +0100

    2022 - day 18

--- 2022/day18/main.clj
+++ 2022/day18/main.clj
@@ -3,6 +3,28 @@
 (defrecord Point [x y z])
 (defrecord Bounds [low high])
 
+;; Point helpers
+(defn point-apply
+  ([f a b]
+   (->Point (f (:x a) (:x b)) (f (:y a) (:y b)) (f (:z a) (:z b))))
+  ([f a]
+   (->Point (f (:x a)) (f (:y a)) (f (:z a)))))
+
+(defn point-cmp [f a b]
+  (list (f (:x a) (:x b)) (f (:y a) (:y b)) (f (:z a) (:z b))))
+
+;; Bounds helpers
+(defn get-bounds [points]
+  (->Bounds
+   (point-apply #(- % 1) (reduce #(point-apply min %1 %2) points))
+   (point-apply #(+ % 1) (reduce #(point-apply max %1 %2) points))))
+
+(defn in-bounds? [p bounds]
+  (and
+   (reduce #(and %1 %2) (point-cmp >= p (:low bounds)))
+   (reduce #(and %1 %2) (point-cmp <= p (:high bounds)))))
+
+;; Input
 (defn parse-input [file] 
   (->>
    (slurp file)
@@ -12,6 +34,7 @@
    (map #(apply ->Point %)) 
    (set)))
 
+;; Exploring cubes
 (defn adjacent [p]
   (list 
    (->Point (+ (:x p) 1) (:y p) (:z p))
@@ -21,31 +44,16 @@
    (->Point (:x p) (:y p) (+ (:z p) 1))
    (->Point (:x p) (:y p) (- (:z p) 1))))
 
-(defn open-sides [points p]
+(defn count-sides [p f]
   (->>
-   (adjacent p)
-   (map #(contains? points %))
-   (map #(if % 0 1))
+   (adjacent p) 
+   (map #(if (f %) 1 0))
    (reduce +)))
 
-(defn point-apply 
-  ([f a b] 
-   (->Point (f (:x a) (:x b)) (f (:y a) (:y b)) (f (:z a) (:z b))))
-  ([f a]
-   (->Point (f (:x a)) (f (:y a)) (f (:z a)))))
-
-(defn point-cmp [f a b]
-  (list (f (:x a) (:x b)) (f (:y a) (:y b)) (f (:z a) (:z b))))
-
-(defn get-bounds [points]
-  (->Bounds 
-   (point-apply #(- % 1) (reduce #(point-apply min %1 %2) points))
-   (point-apply #(+ % 1) (reduce #(point-apply max %1 %2) points))))
-
-(defn in-bounds? [p bounds]
-  (and 
-   (reduce #(and %1 %2) (point-cmp >= p (:low bounds)))
-   (reduce #(and %1 %2) (point-cmp <= p (:high bounds)))))
+(defn count-all [points f]
+  (->>
+   (map #(count-sides % f) points)
+   (reduce +)))
 
 (defn expansion-candidates [this points visited bounds]
   (->>
@@ -61,24 +69,14 @@
           next (expansion-candidates this points visited bounds)]
       (recur (into other next) points (into visited next) bounds)))) 
 
-(defn exposed-sides [points p]
-  (->>
-   (adjacent p)
-   (map #(contains? points %))
-   (map #(if % 1 0))
-   (reduce +)))
-
+;; Solution
 (defn part1 [points]
-  (->>
-   (map #(open-sides points %) points)
-   (reduce +)))
+  (count-all points #(not (contains? points %))))
 
 (defn part2 [points]
   (let [bounds (get-bounds points)
         steam (expand-steam [(:low bounds)] points #{(:low bounds)} bounds)]
-    (->>
-     (map #(exposed-sides steam %) points)
-     (reduce +))))
+    (count-all points #(contains? steam %))))
 
 ;; Main
 (let [input (parse-input "input")]
