eb54b0f941f8a189bad2455e015b61fb6a6fd61e
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-12-21 22:39:17 +0100

    2022 - day 21

--- 2022/day21/main.hs
+++ 2022/day21/main.hs
@@ -1,5 +1,26 @@
 import Numeric
 
+printResult :: Double -> IO ()
+printResult x = putStrLn $ showFFloat Nothing x ""
+
 main :: IO ()
-main = putStrLn $ showFFloat Nothing root "" where
--- {{part1}}
+main = mapM_ printResult [(part1), (part2)]
+
+part1 :: Double
+part1 = root where
+    -- {{part1}}
+
+part2 :: Double
+part2 = findn 0 10000000000000
+
+findn :: Double -> Double -> Double
+findn low high = 
+    let n = low + (high - low) / 2 in
+    case compare (shout n) 0 of
+        GT -> findn n high
+        LT -> findn low n
+        EQ -> n
+
+shout :: Double -> Double
+shout humn = (rvrh - hzgl) where
+    -- {{part2}}
