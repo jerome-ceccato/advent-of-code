bcf4b999fd55c1361428b10cc02ef7c5a6a6876a
Jérôme Ceccato <jerome.ceccato@gmail.com>
2022-12-21 22:39:17 +0100

    2022 - day 21

--- 2022/day21/run.ps1
+++ 2022/day21/run.ps1
@@ -1,4 +1,6 @@
-$inputData = (Get-Content -Raw -Path 'input') -replace ': ', ' = '
+$inputData = ((Get-Content -Raw -Path 'input') -replace ': ', ' = ') -replace '\n',"`n    "
 $template = (Get-Content -Raw -Path 'main.hs') -replace '-- {{part1}}', $inputData
+$part2Input = ($inputData -replace "    root = rvrh \+ hzgl`n",'') -replace "    humn = 2116`n",''
+$template = $template -replace '-- {{part2}}', $part2Input
 $template | Set-Content -Path 'generated.hs'
 runghc .\generated.hs
