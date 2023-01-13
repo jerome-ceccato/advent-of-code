d3bf76b3a101124f3bc596a952cc7a010d3ac5e6
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-01-13 23:33:44 +0100

    2020 - day02

---
+++ 2020/day02/main.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(require threading)
+
+(struct rule (lower upper c password))
+
+(define read-input 
+  (λ~>>
+    (file->lines)
+    (map (λ~>> 
+      (regexp-match #px"(\\d+)-(\\d+) (\\w): (\\w+)")
+      (cdr)
+      ((λ (x) (list 
+        (string->number (first x)) 
+        (string->number (second x)) 
+        (string-ref (third x) 0) 
+        (fourth x))))
+      (apply rule)))))
+
+(define part1
+  (λ~>>
+    (count (λ (r)
+      (~>>
+        (string->list (rule-password r))
+        (filter (λ (c) (eqv? c (rule-c r))))
+        (length)
+        ((λ (x) (and (>= x (rule-lower r)) (<= x (rule-upper r))))))))))
+
+(define part2
+  (λ~>>
+    (count (λ (r)
+      (xor
+        (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-lower r) 1)))
+        (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-upper r) 1))))))))
+ 
+(printf "part 1: ~a~n" (part1 (read-input "input")))
+(printf "part 2: ~a~n" (part2 (read-input "input")))
