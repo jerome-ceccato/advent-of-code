03254087a36f7c0748c04a6620c4434ad3ee9faa
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-01-14 16:50:03 +0100

    2020 - auto indent

--- 2020/day02/main.rkt
+++ 2020/day02/main.rkt
@@ -4,34 +4,41 @@
 
 (struct rule (lower upper c password))
 
-(define read-input 
+(define read-input
   (λ~>>
-    (file->lines)
-    (map (λ~>> 
-      (regexp-match #px"(\\d+)-(\\d+) (\\w): (\\w+)")
-      (cdr)
-      ((λ (x) (list 
-        (string->number (first x)) 
-        (string->number (second x)) 
-        (string-ref (third x) 0) 
-        (fourth x))))
-      (apply rule)))))
+   (file->lines)
+   (map
+    (λ~>>
+     (regexp-match #px"(\\d+)-(\\d+) (\\w): (\\w+)")
+     (cdr)
+     ((λ (x)
+        (list
+         (string->number (first x))
+         (string->number (second x))
+         (string-ref (third x) 0)
+         (fourth x))))
+     (apply rule)))))
+
+(define input (read-input "input"))
 
 (define part1
   (λ~>>
-    (count (λ (r)
+   (count
+    (λ (r)
       (~>>
-        (string->list (rule-password r))
-        (filter (λ (c) (eqv? c (rule-c r))))
-        (length)
-        ((λ (x) (and (>= x (rule-lower r)) (<= x (rule-upper r))))))))))
+       (string->list (rule-password r))
+       (filter (λ (c) (eqv? c (rule-c r))))
+       (length)
+       ((λ (x) (<= (rule-lower r) x (rule-upper r)))))))))
 
 (define part2
   (λ~>>
-    (count (λ (r)
+   (count
+    (λ (r)
       (xor
-        (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-lower r) 1)))
-        (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-upper r) 1))))))))
- 
-(printf "part 1: ~a~n" (part1 (read-input "input")))
-(printf "part 2: ~a~n" (part2 (read-input "input")))
+       (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-lower r) 1)))
+       (eqv? (rule-c r) (string-ref (rule-password r) (- (rule-upper r) 1))))))))
+
+
+(printf "part 1: ~a~n" (part1 input))
+(printf "part 2: ~a~n" (part2 input))
