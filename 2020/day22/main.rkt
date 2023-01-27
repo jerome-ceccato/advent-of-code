868526cd5bfc8809ebd0d526593fcb111756f6a0
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-01-27 21:10:44 +0100

    2020 - day22

--- 2020/day22/main.rkt
+++ 2020/day22/main.rkt
@@ -33,5 +33,33 @@
               [i (in-naturals 1)])
       (* n i))))
 
+(define (play-rec-normal-round c1 c2 p1 p2)
+  (if (> c1 c2)
+      (list (append p1 (list c1 c2)) p2)
+      (list p1 (append p2 (list c2 c1)))))
+
+(define (play-rec-recursive-round c1 c2 p1 p2)
+  (let* ([p1-copy (take p1 c1)]
+         [p2-copy (take p2 c2)]
+         [rec-game-result (play-rec (list p1-copy p2-copy))])
+    (if (eqv? (car rec-game-result) 'p1)
+        (list (append p1 (list c1 c2)) p2)
+        (list p1 (append p2 (list c2 c1))))))
+
+(define (play-rec decks [game-cache (set)])
+  (match-let ([(list p1 p2) decks])
+    (cond
+      [(empty? p1) (list 'p2 p1 p2)]
+      [(empty? p2) (list 'p1 p1 p2)]
+      [(set-member? game-cache decks) (list 'p1 p1 p2)]
+      [else
+       (let ([c1 (car p1)] [c2 (car p2)])
+         (cond
+           [(or (> c1 (length (cdr p1))) (> c2 (length (cdr p2))))
+            (play-rec (play-rec-normal-round c1 c2 (cdr p1) (cdr p2)) (set-add game-cache decks))]
+           [else
+            (play-rec (play-rec-recursive-round c1 c2 (cdr p1) (cdr p2)) (set-add game-cache decks))]))])))
+
+
 (printf "part 1: ~a~n" (~> "input" (read-input) (play) (score)))
-;(printf "part 2: ~a~n" (~> "input" (read-input)))
+(printf "part 2: ~a~n" (~> "input" (read-input) (play-rec) (cdr) (score)))
