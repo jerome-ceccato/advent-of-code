4ab97a5e8c0cc88b129a3b78e81d679947a794f6
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-12-11 23:06:31 +0100

    2023 d11

--- 2023/day11/main.nim
+++ 2023/day11/main.nim
@@ -1,46 +1,56 @@
-import std/sets
 import std/strutils
 import std/sequtils
-import std/sugar
+import std/algorithm
 
 type Point = tuple[x: int, y: int]
 
-proc expandUniverse(lines: var seq[string]): seq[string] =
-    for y in countdown(high(lines), low(lines)):
+proc expandUniverse(rows: seq[int], cols: seq[int], galaxies: var seq[Point], amount: int): seq[Point] =
+    for y in rows.reversed():
+        galaxies = galaxies.mapIt(if it.y > y: (it.x, it.y + amount) else: it)
+    for x in cols.reversed():
+        galaxies = galaxies.mapIt(if it.x > x: (it.x + amount, it.y) else: it)
+    galaxies
+
+proc findExpansionAreas(lines: seq[string]): tuple[rows: seq[int], cols: seq[int]] =
+    var rows = newSeq[int]()
+    var cols = newSeq[int]()
+
+    for y in low(lines)..high(lines):
         if not lines[y].contains('#'):
-            lines.insert(lines[y], y)
-    for x in countdown(high(lines[0]), low(lines[0])):
+            rows.add(y)
+
+    for x in low(lines[0])..high(lines[0]):
         var hasGalaxy = false
         for y in low(lines)..high(lines):
             if lines[y][x] == '#':
                 hasGalaxy = true
         if not hasGalaxy:
-            for y in low(lines)..high(lines):
-                lines[y].insert(".", x)
-    lines
+            cols.add(x)
+    (rows, cols)
 
-proc collectGalaxies(lines: seq[string]): OrderedSet[Point] =
-    var points = initOrderedSet[Point]()
+proc collectGalaxies(lines: seq[string]): seq[Point] =
+    var points = newSeq[Point]()
     for y in low(lines)..high(lines):
         for x in low(lines[y])..high(lines[y]):
             if lines[y][x] == '#':
-                points.incl((x, y))
+                points.add((x, y))
     points
 
-proc parseInput(): OrderedSet[Point] =
+proc prepareInput(universeExpansionFactor: int): seq[Point] =
     var lines = readFile("input").splitLines()
-    lines = expandUniverse(lines)
-    collectGalaxies(lines)
+    let expansionAreas = findExpansionAreas(lines)
+    var galaxies = collectGalaxies(lines)
+    expandUniverse(expansionAreas.rows, expansionAreas.cols, galaxies, universeExpansionFactor - 1)
 
 proc distance(lhs: Point, rhs: Point): int = 
     abs(lhs.x - rhs.x) + abs(lhs.y - rhs.y)
 
-proc countDistances(galaxies: OrderedSet[Point]): int =
+proc countDistances(galaxies: seq[Point]): int =
     var total = 0
-    let garray = galaxies.toSeq()
-    for i in low(garray)..high(garray):
-        for j in i..high(garray):
-            total += distance(garray[i], garray[j])
+    for i in low(galaxies)..high(galaxies):
+        for j in i..high(galaxies):
+            total += distance(galaxies[i], galaxies[j])
     total
 
-echo countDistances(parseInput())
+echo countDistances(prepareInput(2))
+echo countDistances(prepareInput(1000000))
