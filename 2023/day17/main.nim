e5ad36a20fab35624b7399fa047fdee741c9e9c8
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-12-17 17:44:43 +0100

    2023 day17

--- 2023/day17/main.nim
+++ 2023/day17/main.nim
@@ -6,7 +6,7 @@ import std/sets
 type Point = tuple[x: int, y: int]
 type Path = object
     point: Point
-    last3Dir: seq[Point]
+    lastDir: seq[Point]
     loss: int
 
 let ZERO: Point = (x: 0, y: 0)
@@ -19,6 +19,7 @@ proc `<`(a, b: Path): bool = a.loss < b.loss
 proc `+`(a, b: Point): Point = (x: a.x + b.x, y: a.y + b.y)
 proc `-`(a, b: Point): Point = (x: a.x - b.x, y: a.y - b.y)
 proc `-`(a: Point): Point = (x: -a.x, y: -a.y)
+proc `*`(a: Point, b: int): Point = (x: a.x * b, y: a.y * b)
 
 proc parseInput(): seq[seq[int]] =
     let lines = readFile("input").splitLines()
@@ -38,35 +39,71 @@ proc explore(board: seq[seq[int]]): int =
     var queue = initHeapQueue[Path]()
     var visited = initHashSet[(Point, seq[Point])]()
 
-    queue.push(Path(point: ZERO, last3Dir: @[], loss: 0))
+    queue.push(Path(point: ZERO, lastDir: @[], loss: 0))
     
-    var steps = 0
     while queue.len() > 0:
         let current: Path = queue.pop()
         
-        steps += 1
         if current.point == target:
-            echo "steps: ", steps
             return current.loss
 
-        if not visited.contains((current.point, current.last3Dir)):
-            visited.incl((current.point, current.last3Dir))
+        if not visited.contains((current.point, current.lastDir)):
+            visited.incl((current.point, current.lastDir))
 
             for dir in [RIGHT, DOWN, LEFT, UP]:
                 let next = current.point + dir
                 if inBounds(next, board) and 
-                    (current.last3Dir.len() == 0 or dir != -current.last3Dir[^1]) and
-                    not (current.last3Dir.len() == 3 and current.last3Dir.allIt(it == dir)):
+                    (current.lastDir.len() == 0 or dir != -current.lastDir[^1]) and
+                    not (current.lastDir.len() == 3 and current.lastDir.allIt(it == dir)):
                     
-                    var nextDirs = current.last3Dir
+                    var nextDirs = current.lastDir
                     nextDirs.add(dir)
                     if nextDirs.len() > 3:
                         nextDirs.delete(0)
 
                     queue.push(Path(
                         point: next, 
-                        last3Dir: nextDirs, 
+                        lastDir: nextDirs, 
                         loss: current.loss + board[next.y][next.x]))
     -1
 
-echo explore(parseInput())
+proc exploreUltra(board: seq[seq[int]]): int =
+    let target: Point = (x: high(board[high(board)]), y: high(board))
+    var queue = initHeapQueue[Path]()
+    var visited = initHashSet[(Point, seq[Point])]()
+
+    queue.push(Path(point: ZERO, lastDir: @[], loss: 0))
+    
+    while queue.len() > 0:
+        let current: Path = queue.pop()
+        
+        if current.point == target:
+            return current.loss
+
+        if not visited.contains((current.point, current.lastDir)):
+            visited.incl((current.point, current.lastDir))
+
+            for dir in [RIGHT, DOWN, LEFT, UP]:
+                let next = current.point + (dir * 4)
+                if inBounds(next, board) and 
+                    (current.lastDir.len() == 0 or dir != -current.lastDir[0]) and
+                    not (current.lastDir.len() == 1 and current.lastDir[0] == dir):
+                    
+                    var pos = current.point
+                    var loss = current.loss
+                    for _ in 1..3:
+                        pos = pos + dir
+                        loss += board[pos.y][pos.x]
+                    for _ in 4..10:
+                        pos = pos + dir
+                        if inBounds(pos, board):
+                            loss += board[pos.y][pos.x]
+                            queue.push(Path(
+                                point: pos, 
+                                lastDir: @[dir], 
+                                loss: loss))
+    -1
+
+let input = parseInput()
+echo explore(input)
+echo exploreUltra(input)
