b8e60ba6287cc5f6c2f86edb4a04d860dbc7fa0d
Jérôme Ceccato <jerome.ceccato@gmail.com>
2023-12-20 18:11:26 +0100

    2023 d20p2 dirty

--- 2023/day20/main.nim
+++ 2023/day20/main.nim
@@ -2,6 +2,8 @@ import std/strutils
 import std/sequtils
 import std/tables
 import std/deques
+import std/sets
+import std/math
 
 type ModuleKind = enum
     flipflop, conjunction, broadcaster, none
@@ -25,6 +27,9 @@ type ConjunctionModule = ref object of Module
 
 type BroadcasterModule = ref object of Module
 
+type FinalModule = ref object of Module
+    done: bool = false
+
 method linkInput(self: Module, input: string) {.base.} =
     self.inputs.add(input)
 
@@ -34,7 +39,6 @@ method linkInput(self: ConjunctionModule, input: string) =
 
 method receive(self: Module, action: Action): seq[Action] {.base.} =
     discard
-    #echo "Received ", action.pulse, " from ", action.source
 
 method receive(self: BroadcasterModule, action: Action): seq[Action] =
     self.destinations.mapIt(Action(pulse: action.pulse, source: self.label, destination: it))
@@ -50,6 +54,29 @@ method receive(self: ConjunctionModule, action: Action): seq[Action] =
     self.state[action.source] = action.pulse
     let output = not self.state.values.toSeq.allIt(it)
     self.destinations.mapIt(Action(pulse: output, source: self.label, destination: it))
+
+method receive(self: FinalModule, action: Action): seq[Action] =
+    if not action.pulse:
+        self.done = true
+    @[]
+
+method debugValue(self: Module): string {.base.} =
+    "  "
+
+method debugValue(self: FlipFlopModule): string =
+    if self.state:
+        "# "
+    else:
+        ". "
+
+method debugValue(self: ConjunctionModule): string =
+    var on = self.state.values.toSeq.countIt(it)
+    var output = $on
+    if output.len() == 1:
+        output &= " "
+    if on == self.state.len():
+        output = "[]"
+    output
 
 proc makeModule(label: string, kind: ModuleKind, destinations: seq[string]): Module =
     case kind
@@ -113,5 +140,110 @@ proc warmUp(network: TableRef[string, Module]): int =
         pulse_count[1] += results[1]
     pulse_count[0] * pulse_count[1]
 
-let input = parseInput()
-echo warmUp(input)
+proc press2(network: TableRef[string, Module], step: int, period: TableRef[string, int]) =
+    var queue = [Action(pulse: false, source: "button", destination: "broadcaster")].toDeque
+    while queue.len() > 0:
+        let action = queue.popFirst()
+        if action.pulse and action.destination == network["rx"].inputs[0]:
+            if not period.hasKey(action.source):
+                period[action.source] = step
+        let next = network[action.destination].receive(action)
+        for item in next:
+            queue.addLast(item)
+
+proc startRx(network: TableRef[string, Module]): int =
+    var steps = 0
+    var period = newTable[string, int]()
+    let target = network["rx"].inputs[0]
+    let targetCount = network[target].inputs.len()
+    while period.len() != targetCount:
+        steps += 1
+        press2(network, steps, period)
+    
+    period.values.toSeq().foldl(lcm(a, b))
+
+#echo warmUp(parseInput())
+echo startRx(parseInput())
+
+
+proc sendPulse(network: TableRef[string, Module], pulse: Action) =
+    var queue = [pulse].toDeque
+    while queue.len() > 0:
+        let action = queue.popFirst()
+        let next = network[action.destination].receive(action)
+        for item in next:
+            queue.addLast(item)
+
+# var network = parseInput()
+# var state = false
+# var loops = 0
+# while true:
+#     sendPulse(network, Action(pulse: false, source: "broadcaster", destination: "gz"))
+#     loops += 1
+#     if cast[ConjunctionModule](network["kl"]).state["xt"] != state:
+#         state = not state
+#         echo "Changed state after ", loops, "(", state, ")"
+#     if loops mod 10000 == 0:
+#         echo loops
+    
+
+# All 4 loops from the broadcaster are only contain `kl` and `rx` in common
+# kl is a conjunction to pulse rx
+# So we can analyze all 4 loops separately, find when they send a high pulse to kl, and infer the answer that way
+
+proc getLinks(network: TableRef[string, Module], origin: string): HashSet[string] =
+    var links = initHashSet[string]();
+    var queue = [origin].toDeque()
+    links.incl(origin)
+    while queue.len() > 0:
+        let current = queue.popFirst()
+        for dest in network[current].destinations:
+            if not links.contains(dest):
+                links.incl(dest)
+                queue.addLast(dest)
+    links
+
+proc dump(network: TableRef[string, Module], showNames: bool) = 
+    const sorted_labels = ["kl", "gv", "xt", "gz", "mz", "ps", "kx", "ss", "lq", "tz", "qh", "vk", "gk", "lb", "gx"]
+    var names = ""
+    var values = ""
+    for label in sorted_labels:
+        let lb = if label == "broadcaster": ">>" else: label
+        let value = network[label].debugValue()
+        
+        names &= " | " & lb
+        values &= " | " & value
+    if showNames:
+        echo names
+    echo values
+
+# let links = getLinks(network, "gz")
+# var snetwork = newTable[string, Module]()
+# for label in links:
+#     snetwork[label] = network[label]
+
+
+# snetwork.dump(true)
+# for i in 1..(2^13):
+#     sendPulse(snetwork, Action(pulse: false, source: "broadcaster", destination: "gz"))
+#     #snetwork.dump(false)
+# snetwork.dump(true)
+# for _ in 1..(2^11-1):
+#     sendPulse(snetwork, Action(pulse: false, source: "broadcaster", destination: "gz"))
+# snetwork.dump(false)
+# echo "----------"
+# for _ in 1..(2^11-2):
+#     sendPulse(snetwork, Action(pulse: false, source: "broadcaster", destination: "gz"))
+
+# sendPulse(snetwork, Action(pulse: false, source: "broadcaster", destination: "gz"))
+# snetwork.dump(false)
+# sendPulse(snetwork, Action(pulse: false, source: "broadcaster", destination: "gz"))
+# snetwork.dump(false)
+# for dest in network["broadcaster"].destinations:
+#     let res = getLinks(network, dest)
+#     echo res
+#     for dest2 in network["broadcaster"].destinations:
+#         if dest != dest2:
+#             echo "intersect(", dest2, "): ", res.intersection(getLinks(network, dest2))
+
+# higher than 3085000
